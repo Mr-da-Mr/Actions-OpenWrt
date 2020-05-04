4a04b04d9d45f6026f7140e97d5edd02106095d2
Mr-da-Mr <63508593+Mr-da-Mr@users.noreply.github.com>
2020-05-04 23:15:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,12 +12,17 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package
-#sed -i '$a src-git clone https://github.com/vernesong/OpenClash
-#sed -i '$a src-git clone https://github.com/rufengsuixing/luci-app-adguardhome
-#sed -i '$a src-git clone https://github.com/tzxiaozhen88/koolproxyR
-#sed -i '$a src-git clone https://github.com/ElonH/Rclone-OpenWrt
-#sed -i '$a src-git clone https://github.com/jerrykuku/luci-theme-argon
-#sed -i '$a src-git clone https://github.com/longcat99/opdiy/tree/master/diy/luci-app-passwall
-#sed -i '$a src-git clone https://github.com/fw876/helloworld
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/vernesong/OpenClash.git ./package/OpenClash
+git clone https://github.com/tzxiaozhen88/koolproxyR ./package/koolproxyR
+git clone https://github.com/ElonH/Rclone-OpenWrt ./package/Rclone-OpenWrt
+git clone https://github.com/jerrykuku/luci-theme-argon
+git clone https://github.com/longcat99/opdiy/tree/master/diy/luci-app-passwall
+git clone https://github.com/fw876/helloworld
+git clone https://github.com/mchome/openwrt-vlmcsd.git ./package/openwrt-vlmcsd
+git clone https://github.com/mchome/luci-app-vlmcsd.git ./package/luci-app-vlmcsd
+git clone https://github.com/kuoruan/openwrt-v2ray.git ./package/openwrt-v2ray
+git clone https://github.com/kuoruan/luci-app-v2ray.git ./package/luci-app-v2ray
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/luci-app-adguardhome
+./scripts/feeds update -a
+./scripts/feeds install -a

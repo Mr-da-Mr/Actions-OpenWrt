8009257ef3960fa5bc5a83446c0c29c0ff3c2146
Mr-da-Mr <63508593+Mr-da-Mr@users.noreply.github.com>
2020-05-05 11:26:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,13 +12,13 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/vernesong/OpenClash.git ./package/OpenClash
 git clone https://github.com/tzxiaozhen88/koolproxyR ./package/koolproxyR
 git clone https://github.com/ElonH/Rclone-OpenWrt ./package/Rclone-OpenWrt
 git clone https://github.com/jerrykuku/luci-theme-argon
 git clone https://github.com/longcat99/opdiy/tree/master/diy/luci-app-passwall
-git clone https://github.com/fw876/helloworld
+#git clone https://github.com/fw876/helloworld
 git clone https://github.com/mchome/openwrt-vlmcsd.git ./package/openwrt-vlmcsd
 git clone https://github.com/mchome/luci-app-vlmcsd.git ./package/luci-app-vlmcsd
 git clone https://github.com/kuoruan/openwrt-v2ray.git ./package/openwrt-v2ray

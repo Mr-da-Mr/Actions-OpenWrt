08644418f885fe1b3423b1b4012e3b9472e6aa60
Mr-da-Mr <63508593+Mr-da-Mr@users.noreply.github.com>
2020-05-06 10:16:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,16 +13,16 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone -b master --depth 1 https://github.com/kuoruan/openwrt-upx.git package/openwrt-upx
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 git clone https://github.com/tzxiaozhen88/koolproxyR package/koolproxyR
 git clone https://github.com/ElonH/Rclone-OpenWrt package/Rclone-OpenWrt
-git clone https://github.com/jerrykuku/luci-theme-argon
+#git clone https://github.com/jerrykuku/luci-theme-argon
 git clone https://github.com/longcat99/opdiy/tree/master/diy/luci-app-passwall
 #git clone https://github.com/fw876/helloworld
 git clone https://github.com/mchome/openwrt-vlmcsd.git package/openwrt-vlmcsd
 git clone https://github.com/mchome/luci-app-vlmcsd.git package/luci-app-vlmcsd
 #git clone https://github.com/kuoruan/openwrt-v2ray.git package/openwrt-v2ray
-git clone -b master --depth 1 https://github.com/kuoruan/openwrt-upx.git package/openwrt-upx
 git clone https://github.com/kuoruan/luci-app-v2ray.git package/luci-app-v2ray
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 ./scripts/feeds update -a

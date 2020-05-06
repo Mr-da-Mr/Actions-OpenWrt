29a4f614636e226c2a606879f1892bdcdd2201f5
Mr-da-Mr <63508593+Mr-da-Mr@users.noreply.github.com>
2020-05-06 10:13:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,7 +21,7 @@ git clone https://github.com/longcat99/opdiy/tree/master/diy/luci-app-passwall
 #git clone https://github.com/fw876/helloworld
 git clone https://github.com/mchome/openwrt-vlmcsd.git package/openwrt-vlmcsd
 git clone https://github.com/mchome/luci-app-vlmcsd.git package/luci-app-vlmcsd
-git clone https://github.com/kuoruan/openwrt-v2ray.git package/openwrt-v2ray
+#git clone https://github.com/kuoruan/openwrt-v2ray.git package/openwrt-v2ray
 git clone -b master --depth 1 https://github.com/kuoruan/openwrt-upx.git package/openwrt-upx
 git clone https://github.com/kuoruan/luci-app-v2ray.git package/luci-app-v2ray
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
